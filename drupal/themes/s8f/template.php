3aa09b369f78129ddbfccbc16d07995e3f6adc24
Carlos Rodriguez <carlos@s8f.org>
2011-01-01 16:11:13 -0800

    Stripping down the theme again. Larger font, on white background.

--- drupal/themes/s8f/template.php
+++ drupal/themes/s8f/template.php
@@ -2,14 +2,6 @@
 // $Id$
 
 function s8f_preprocess_html(&$variables) {
-  $body_bgs = glob(drupal_get_path('theme', 's8f') . '/images/body-backgrounds/*');
-  $body_bg = base_path() . $body_bgs[mt_rand(0, count($body_bgs) - 1)];
-  $header_bgs = glob(drupal_get_path('theme', 's8f') . '/images/header-backgrounds/*');
-  $header_bg = base_path() . $header_bgs[mt_rand(0, count($header_bgs) - 1)];
-  $css = <<<EOT
-body { background-image: url($body_bg); }
-#header { background-image: url($header_bg); }
-EOT;
-  drupal_add_css($css, array('type' => 'inline', 'group' => CSS_THEME));
+  
 }
 
