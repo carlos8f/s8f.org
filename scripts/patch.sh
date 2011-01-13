559c4d3f2dcdd417c3e37db0d55119409fd34726
Carlos Rodriguez <carlos@s8f.org>
2011-01-12 22:50:57 -0800

    Tweaked patch.sh so commit message is optional.

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # patch.sh -- a patch creation script for git.
 #
@@ -9,7 +9,25 @@
 # author: Carlos Rodriguez <carlos@s8f.org>
 # url: http://s8f.org/software
 # license: MIT
-#
+
+## Parse arguments.
+versus="master"
+m_flag=
+while getopts 'm:' OPTION
+do
+  case $OPTION in
+  m)  m_flag=1
+      message="$OPTARG"
+        ;;
+  ?)  printf "Usage: %s: [-m message] [<versus>]\n" $(basename $0) >&2
+        exit 2
+        ;;
+  esac
+done
+shift $((OPTIND - 1))
+if [ "$1" ]; then
+  versus=$1;
+fi
 
 ## Check for uncommitted changes which would be lost in the process.
 git diff --exit-code --numstat > /dev/null
@@ -18,33 +36,30 @@ if [ $? -eq 1 ]; then
   exit 1;
 fi
 
-## Get the current branch and starting point.
+## Get the current branch.
 branch=`git branch | grep '*' | awk '{print $2; exit}'`
-remote="master"
-if [ -z "$1" ]; then
-  echo "Usage: patch.sh <patch description> [<versus>]"
-  exit 1
-fi
-if [ $2 ]; then
-  remote=$2
-fi;
-if [ $branch = $remote ]; then
+if [ $branch = $versus ]; then
   echo "Cannot diff $branch with itself. Please specify a <versus> different from the current branch."
   exit 1
 fi
 
 ## Check for differences.
-git diff --exit-code --numstat $remote > /dev/null
+git diff --exit-code --numstat $versus > /dev/null
 if [ $? -eq 0 ]; then
-  echo "No differences with $remote found. Aborting."
+  echo "No differences with $versus found. Aborting."
   exit 1
 fi
 
 ## Create a throwaway branch for merging.
-git checkout -q -b patch_tmp $remote > /dev/null
+git checkout -q -b patch_tmp $versus > /dev/null
 ## Squash history into a single commit.
 git merge -q --squash $branch > /dev/null
-git commit -q -a -m "$1"
+if [ "$m_flag" ]; then
+  git commit -q -a -m "$message"
+else
+  git commit -q -a
+fi
+
 ## Create the patch.
 git format-patch --no-prefix -1 -b -B --inter-hunk-context=20 --stdout
 ## Return to the original branch and clean up.
