e88f5097ce9b08c04f066b6163bc30cd650c6c94
Carlos Rodriguez <carlos8f>
2011-01-13 19:47:41 -0800

    Test commit.

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -10,67 +10,27 @@
 # url: http://s8f.org/software
 # license: MIT
 
-## Options for git format-patch:
-patch_opts="-1 --stdout --no-prefix -b -B --inter-hunk-context=20"
+message=""
+capture_message=0
+diff_opts=""
+log_opts=""
 
 ## Parse arguments.
-versus="master"
-m_flag=
-while getopts 'm:' OPTION
+for arg in "$@"
 do
-  case $OPTION in
-  m)  m_flag=1
-      message="$OPTARG"
-        ;;
-  ?)  printf "Usage: %s: [-m message] [<versus>]\n" $(basename $0) >&2
-        exit 2
-        ;;
-  esac
+  if [[ $arg == -* ]]; then
+    if [ $arg = "-m" ]; then
+      capture_message=1
+    else
+      diff_opts="$diff_opts $arg"
+    fi
+  elif [ $capture_message -eq 1 ]; then
+    message=$arg
+    capture_message=0
+  else
+    log_opts="$log_opts $arg"
+  fi
+  shift
 done
-shift $((OPTIND - 1))
-if [ "$1" ]; then
-  versus=$1;
-fi
 
-git status -s &> /dev/null
-if [ $? -gt 0 ]; then
-  echo "Current directory is not a git repository!"
-  exit 1
-fi
-
-## Check for uncommitted changes which would be lost in the process.
-git diff --exit-code --numstat > /dev/null
-if [ $? -eq 1 ]; then
-  echo "You have uncommitted changes. Please commit or stash them first."
-  exit 1
-fi
-
-## Get the current branch.
-branch=`git branch | grep '*' | awk '{print $2; exit}'`
-if [ $branch = $versus ]; then
-  echo "Cannot diff $branch with itself. Please specify a <versus> different from the current branch."
-  exit 1
-fi
-
-## Check for differences.
-git diff --exit-code --numstat $versus > /dev/null
-if [ $? -eq 0 ]; then
-  echo "No differences with $versus found. Aborting."
-  exit 1
-fi
-
-## Create a throwaway branch for merging.
-git checkout -q -b patch_tmp $versus > /dev/null
-## Squash history into a single commit.
-git merge -q --squash $branch > /dev/null
-if [ "$m_flag" ]; then
-  git commit -q -a -m "$message"
-else
-  git commit -q -a
-fi
-
-## Create the patch.
-git format-patch $patch_opts
-## Return to the original branch and clean up.
-git checkout -q $branch
-git branch -D patch_tmp > /dev/null
+git log --pretty=oneline $log_opts
