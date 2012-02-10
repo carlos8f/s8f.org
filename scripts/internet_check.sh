fc7eb937f3ed9acb444b74ec037d78c9c85d951e
Carlos Rodriguez <carlos@s8f.org>
2012-02-09 17:12:55 -0800

    New internet down script.

--- scripts/internet_check.sh
+++ scripts/internet_check.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+DOWN=0
 while true; do
   if ! ping -w 5 208.67.222.222 >/dev/null; then
-    play -q /home/carlos8f/projects/s8f.org/scripts/alert.wav
+    if [ $DOWN -eq 0 ]; then
+      play -q /home/carlos8f/projects/s8f.org/scripts/xfiles.wav repeat 10 &
+    fi
     logger 'Internet down!'
+    DOWN=1
+  elif [ $DOWN -eq 1 ]; then
+    killall play
+    DOWN=0
   fi
   sleep 5
 done
