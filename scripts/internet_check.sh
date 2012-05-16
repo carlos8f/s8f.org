1d4fe5345ba2accc32d84b9c208ab2e140de36ab
Carlos Rodriguez <carlos@s8f.org>
2012-05-16 12:58:05 -0700

    Tweaks to internet check

--- scripts/internet_check.sh
+++ scripts/internet_check.sh
@@ -8,9 +8,12 @@ while true; do
   # Ping something every 5 seconds
   if ! ping -w 5 208.67.222.222 >/dev/null; then
     # Freak out!
-    if [ $DOWN -eq 0 ]; then
+    if [ "$DOWN" -eq "0" ]; then
       # Play sound if not already playing.
+      DISPLAY=:0 notify-send 'Internet down!' 'You hear eerie whistling...'
       play -q /home/carlos8f/projects/s8f.org/scripts/xfiles.wav repeat 10 &
+    else
+      DISPLAY=:0  notify-send 'Internet down!' 'Time to get some coffee...'
     fi
     logger 'Internet down!'
     DOWN=1
